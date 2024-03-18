9ff18a92bc11f453bedeae85e32f4f35b30e69fd
kymial <nisijamaren@gmail.com>
2024-03-18 12:50:52 +0900

    20240318_3

--- webworkspace/JS/5.JS_Data_type&Variable/data_type.js
+++ webworkspace/JS/5.JS_Data_type&Variable/data_type.js
@@ -2,71 +2,111 @@
     데이터 타입
 
     원시 타입
-    - 
-    - 
-    - 
-    - 
-    - 
-    - sybol
+    - number
+    - string
+    - boolean
+    - null
+    - undefined
+    - symbol
 
     객체 타입
-    - 
+    - object
 */
 
 /*
-    number : 
+    number : 정수 및 실수를 모두 포함하는 타입
 */
-
+var integer = 10;
+var double = 10.0;
 
 
 /* 
-    - 
+    - number 값들은 표기법만 다르고 값은 같을 수 있따.
 */
+ // console.log(integer == double);
 
+/*
+    == : 값만 맞을 시 TRUE
+    === : 데이터 타입까지 맞아야지 TRUE
+*/
 
 
 /*
-    Infinity : 
-    -Infinity : 
-    NaN : 
+    Infinity : 양의 무한대
+    -Infinity : 음의 무한대
+    NaN : 숫자가 아님
 */
+var plus_inf = 1 / 0;
+ // console.log(plus_inf);
 
+var minus_inf = -1 / 0;
+ // console.log(minus_inf);
+
+var nan = 1 * 'string';
+// console.log(nan);
+
+/* '1' =1 로 계산한다. */
 
 
 /*
     string : 
-    - 
-    - 
+    - 문자열 텍스트 타입
+        - "a", 'a', `a` 으로 표현 가능
+        - 일반적으로 javascript에서는 "으로 문자열 표기
 */
-
+var name_ = "홍길동";
+name_ = '홍길동';
+name_ = `홍길동`;
 
 
 /*
-    - 
+    - javascript의 문자열은 배열처럼 인덱스를 이용하여 접근할 수 있다.(유사 배열)
 */
+for(var index = 0; index < name_.length; index++) {
+    //console.log(name_[index]);
+}
 
+/*
+    - 문자열 데이터는 불변성을 가지고 있다. (변경 불가능)
+*/
+name_[0] = '고';
+// console.log(name_);
 
 
 /*
     boolean : 
-    - 
+    - 논리 참, 거짓을 나타내는 타입
+        -  빈 문자열, null, undefined, ㅡㅇㄴ
 */
-
+name_= '';
+if(name_){
+    // console.log('name은 문자열이 아닙니다.')
+}
 
 
 /*
     undefined : 
-    - 
-    - 
+    - 선언 이후에 값을 초기화 하지 않은 변수는 undefined를 가짐
+        - 선언은 되었지만 값을 할당하지 않은 변수
+        - 존재하지 않는 객체 속성
 */
-
+var variable;
+console.log(variable);
+var obj = {};
+console.log(obj.age);
 
 
 /*
     null : 
-    - 
-    - 
-    - 
+    - 의도적으로 변수에 값이 없다는 것을 명시하는 타입
+        - 변수에 저장된 메모리 주소를 제거하는 용도
+        - 함수의 반환값으로 유효한 값이 없을 때 반환하는 용도
+        - null 값은 typeof 타입 확인 연산자로 확인하면 object 타입이 반환됨(단순한 자바스크립트 문제)
 */
+
+obj = null;
+
+// var element = document.getElementsByClassName('.class');
+console.log(element);
 
 

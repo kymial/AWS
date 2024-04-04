78689adbff4ee898e1362ccba3b1dbef0836f528
kymial <nisijamaren@gmail.com>
2024-04-04 17:13:56 +0900

    20240404_0513

--- springwrokspace/board/src/main/java/com/example/board/controller/UserController.java
+++ springwrokspace/board/src/main/java/com/example/board/controller/UserController.java
@@ -2,18 +2,27 @@ package com.example.board.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.board.dto.request.auth.SignUpRequestDto;
+import com.example.board.dto.request.user.PatchNicknameRequestDto;
+import com.example.board.dto.response.ResponseDto;
 import com.example.board.dto.response.user.GetUserResponseDto;
+import com.example.board.service.AuthService;
 import com.example.board.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+
+
 public class UserController {
     
     private final UserService userService;
@@ -26,4 +35,12 @@ public class UserController {
         return response;
     }
 
+    @PatchMapping("/nickname")
+    public ResponseEntity<ResponseDto> patchNickname(
+        @RequestBody @Valid PatchNicknameRequestDto requestBody
+    ){
+        ResponseEntity<ResponseDto> response = userService.patchNickname(requestBody);
+        return response;
 }
+
+    }

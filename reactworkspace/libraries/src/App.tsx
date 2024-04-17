afefefc08c28cfe660c5af86e926ff9a481c9aae
kymial <nisijamaren@gmail.com>
2024-04-17 12:55:00 +0900

    20240417_1254

---
+++ reactworkspace/libraries/src/App.tsx
@@ -0,0 +1,26 @@
+import React  from 'react';
+import ZustandLibrary from 'src/zustand/ZustandLibrary';
+import './App.css';
+import { Route, Routes } from 'react-router';
+import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
+
+
+function App() {
+    return (
+      <Routes>
+      {/* <Route index element={<ReactRouterLibrary />} />
+      <Route path='/page1' element={<ReactRouterPage1 />} />
+      <Route path='/page2' element={<ReactRouterPage2 />} />
+      <Route path='/zustand' element={<ZustandLibrary />} /> */}
+
+      <Route path='/router'>
+        <Route index element={<ReactRouterLibrary />} />
+        <Route path='page1' element={<ReactRouterPage1 />} />
+        <Route path='page2' element={<ReactRouterPage2 />} />
+      </Route>
+      <Route path='/zustand' element={<ZustandLibrary />} />
+    </Routes>
+  );
+}
+
+export default App;

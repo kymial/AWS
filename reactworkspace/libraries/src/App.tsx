f732e5c245f728d6dc6d1259237169ad67dfca03
kymial <nisijamaren@gmail.com>
2024-04-18 14:07:59 +0900

    20240418_1407

--- reactworkspace/libraries/src/App.tsx
+++ reactworkspace/libraries/src/App.tsx
@@ -3,6 +3,9 @@ import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import './App.css';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
+import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
+import ReactCookieLibrary from './react-cookie/ReactCookieLibrary';
 
 
 function App() {
@@ -13,12 +16,15 @@ function App() {
       <Route path='/page2' element={<ReactRouterPage2 />} />
       <Route path='/zustand' element={<ZustandLibrary />} /> */}
 
-      <Route path='/router'>
+      <Route path='/router' element={ <Layout /> }>
         <Route index element={<ReactRouterLibrary />} />
-        <Route path='page1' element={<ReactRouterPage1 />} />
+        <Route path='page1/:value' element={<ReactRouterPage1 />} />
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
+      <Route path='/axios' element={<AxiosLibrary />} />
+      <Route path='/cookie/cookie' element={<ReactCookieLibrary  />} />
+      <Route path='*' element={<h3>404 NOT FOUND</h3>} />
     </Routes>
   );
 }

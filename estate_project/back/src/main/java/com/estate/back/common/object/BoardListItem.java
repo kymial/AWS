ed46973164bb4d1c6a50525fbafed65ec8ea3326
kymial <nisijamaren@gmail.com>
2024-05-02 17:00:52 +0900

    20240502_1700

---
+++ estate_project/back/src/main/java/com/estate/back/common/object/BoardListItem.java
@@ -0,0 +1,50 @@
+package com.estate.back.common.object;
+
+import com.estate.back.entity.BoardEntity;
+
+import lombok.Getter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+@Getter
+
+public class BoardListItem {
+    private Integer receptionNumber;
+    private Boolean status;
+    private String title;
+    private String writerId;
+    private String writeDatetime;
+    private Integer viewCount;
+
+    private BoardListItem(BoardEntity boardEntity) throws Exception {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        Date datetime = simpleDateFormat.parse(boardEntity.getWriteDatetime());
+        simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        String writeDatetime = simpleDateFormat.format(datetime);
+
+        String writerId = boardEntity.getWriterId();
+        writerId = writerId.substring(0, 1) + "*".repeat(writerId.length() - 1);
+
+
+        this.receptionNumber = boardEntity.getReceptionNumber();
+        this.status = boardEntity.getStatus();
+        this.title = boardEntity.getTitle();
+        this.writerId = writerId;
+        this.writeDatetime = writeDatetime;
+        this.viewCount = boardEntity.getViewCount();
+    }
+
+    public static List<BoardListItem> getList(List<BoardEntity> boardEntities) throws Exception {
+        List<BoardListItem> boardList = new ArrayList<>();
+
+        for(BoardEntity boardEntity: boardEntities) {
+            BoardListItem boardListItem = new BoardListItem(boardEntity);
+            boardList.add(boardListItem);
+        }
+        return boardList;
+    }
+}
--- estate_project/back/src/main/java/com/estate/back/service/BoardService.java
+++ estate_project/back/src/main/java/com/estate/back/service/BoardService.java
@@ -4,9 +4,11 @@ import org.springframework.http.ResponseEntity;
 
 import com.estate.back.dto.request.board.PostBoardRequestDto;
 import com.estate.back.dto.response.ResponseDto;
+import com.estate.back.dto.response.board.GetBoardListResponseDto;
 
 public interface BoardService {
     
     ResponseEntity<ResponseDto> postBoard(PostBoardRequestDto dto, String UserId);
+    ResponseEntity<? super GetBoardListResponseDto> getBoardList();
 
 }
